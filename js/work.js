eb9544b99021b80ab4d25a2c8650c1dc61483287
jihyung <coolishh@naver.com>
2025-07-14 19:34:33 +0900

    favicon added

--- js/work.js
+++ js/work.js
@@ -3,16 +3,16 @@ var currentIndex = $('div.active').index() + 1;
 
 $('.num').html('' + currentIndex + '/' + totalItems + '');
 
-$('#myCarousel').bind('slid', function () {
+$('#workCarousel').bind('slid', function () {
     currentIndex = $('div.active').index() + 1;
     $('.num').html('' + currentIndex + '/' + totalItems + '');
 });
 
-$('#myCarousel').on('slide.bs.carousel', function () {
+$('#workCarousel').on('slide.bs.carousel', function () {
     currentIndex = $('div.active').index() + 1;
     $('.num').html('' + currentIndex + '/' + totalItems + '');
 });
 
-// $('#myCarousel').carousel({
+// $('#workCarousel').carousel({
 //     interval: 2000
 // });
