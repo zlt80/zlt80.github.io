30e4db0d36a46a5acf7b6dc6f54700e84b4a7dcf
jihyung <coolishh@naver.com>
2025-07-14 21:36:02 +0900

    carousel js added for 2 parts in work page

--- js/work.js
+++ js/work.js
@@ -1,18 +1,37 @@
-var totalItems = $('.item').length;
-var currentIndex = $('div.active').index() + 1;
+var totalItems = $('#workCarousel .item').length;
+var currentIndex = $('#workCarousel div.active').index() + 1;
 
-$('.num').html('' + currentIndex + '/' + totalItems + '');
+$('#workCarousel .num').html('' + currentIndex + '/' + totalItems + '');
 
 $('#workCarousel').bind('slid', function () {
-    currentIndex = $('div.active').index() + 1;
-    $('.num').html('' + currentIndex + '/' + totalItems + '');
+    currentIndex = $('#workCarousel div.active').index() + 1;
+    $('#workCarousel .num').html('' + currentIndex + '/' + totalItems + '');
 });
 
 $('#workCarousel').on('slide.bs.carousel', function () {
-    currentIndex = $('div.active').index() + 1;
-    $('.num').html('' + currentIndex + '/' + totalItems + '');
+    currentIndex = $('#workCarousel div.active').index() + 1;
+    $('#workCarousel .num').html('' + currentIndex + '/' + totalItems + '');
 });
 
 // $('#workCarousel').carousel({
+//     interval: 2000
+// });
+
+var totalItems2 = $('#brandCarousel .item').length;
+var currentIndex2 = $('#brandCarousel div.active').index() + 1;
+
+$('#brandCarousel .num').html('' + currentIndex2 + '/' + totalItems2 + '');
+
+$('#brandCarousel').bind('slid', function () {
+    currentIndex2 = $('#brandCarousel div.active').index() + 1;
+    $('#brandCarousel .num').html('' + currentIndex2 + '/' + totalItems2 + '');
+});
+
+$('#brandCarousel').on('slide.bs.carousel', function () {
+    currentIndex2 = $('#brandCarousel div.active').index() + 1;
+    $('#brandCarousel .num').html('' + currentIndex2 + '/' + totalItems2 + '');
+});
+
+// $('#brandCarousel').carousel({
 //     interval: 2000
 // });
